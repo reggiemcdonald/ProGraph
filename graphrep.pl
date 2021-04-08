49b502a98d4f9ea61b499723ed6fc4391ce5c623
julian mentasti <julian@mentasti.net>
2021-04-07 17:48:37 -0700

    initial

--- graphrep.pl
+++ graphrep.pl
@@ -2,6 +2,8 @@
 % our approach uses a graph-term from.
 % Design initially borrowed from prologsite.
 
+% :- dynamc graph/3.
+
 % A GRAPH is defined by graph(NAME,[NODE],[EDGE],[GRAPH]) where:
 % - NAME is a globally unique string.
 % - NODE is defined by node(NAME, [EDGE]) where:
@@ -39,7 +41,7 @@ graph(ex2,
     [
         dEdge(y,z),
         dEdge(z,a),
-        eEdge(a,b),
+        dEdge(a,b),
         dEdge(b,a)
     ],
     [
@@ -57,8 +59,47 @@ graph(ex2,
 
 % Graphs can have properties that are calculated by the following predicates:
 
+% Len(I,N): the number of edges in a graph.
+% - I is the graphID
+% - N is the number of edges
+len(I, N) :- graph(I, _, L2,_), len(L2, N).
+len([], N) :- N is 0.
+len([_|T], N) :-
+    len(T, N1),
+    N is N1+1.
+
+% dis(I, N1, N2, N): the distance from N1 to N2 in graph I.
+% - I is the graphID
+% - N1/N2 is From/To Node name
+% - N is the distance
+dis(I, N1, N2, N) :-
+    graph(I, _, EDGES, _),
+    path(N1, N2, EDGES, [], N).
+
+% path(I, N1, N2, PATH): the path from N1 to N2 in graph I.
+% This is broken idk why I can find the length but not the path.....
+% path(I, N1, N2, PATH) :-
+%     graph(I, _, EDGES, _),
+%     path1(N1, N2, EDGES, PATH).
+
+% path1(FROM,FROM,_,[FROM]).
+% path1(FROM, TO, [dEdge(FROM,NEXT)|EDGES], VISITED) :-
+%     \+ member(FROM, VISITED),
+%     path1(NEXT,TO,EDGES,VISITED1),
+%     append(VISITED1, [FROM], VISITED).
+
+path(FROM,FROM,_,_,N) :- N is 0.
+path(FROM, TO, [dEdge(FROM,NEXT)|EDGES], VISITED, N) :-
+    \+ member(FROM, VISITED),
+    path(NEXT,TO,EDGES,[FROM|VISITED],N1),
+    N is N1 + 1.
+
+
+
+% Path
+
 % Eccentricity: the max distance from a node to all other node.
-% e(I) :-
+% e(I) :- 
 
 % Radius: The min eccentricity from all the edges of the Graph.
 % rad(I) :-
