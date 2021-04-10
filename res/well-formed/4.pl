2cd834e1bf336b38ac26d0a289e051da95d4bff3
kydas <kadasilva22@gmail.com>
2021-04-10 08:50:30 -0700

    Added tests for cycle

---
+++ res/well-formed/4.pl
@@ -0,0 +1,39 @@
+same(Parsed) :-
+    isSame(Parsed, graph(toplevel,
+                         [
+                             node('A', [dEdge('A', 'D'), dEdge('A', 'B')]),
+                             node('B', [dEdge('B', 'C')]),
+                             node('C', [dEdge('C', 'A')]),
+                             node('D', [dEdge('D', 'E')]),
+                             node('E', [])
+                         ],
+                         [
+                             dEdge('A', 'D'),
+                             dEdge('A', 'B'),
+                             dEdge('B', 'C'),
+                             dEdge('C', 'A'),
+                             dEdge('D', 'E')
+                         ],
+                         [
+                             graph(noCycle,
+                                   [
+                                       node('D', [dEdge('D', 'E')]),
+                                       node('E', [])
+                                   ],
+                                   [
+                                       dEdge('D', 'E')
+                                   ],
+                                   []),
+                             graph(cycle1,
+                                   [
+                                       node('A', [dEdge('A', 'D'), dEdge('A', 'B')]),
+                                       node('B', [dEdge('B', 'C')]),
+                                       node('C', [dEdge('C', 'A')])
+                                   ],
+                                   [
+                                       dEdge('A', 'B'),
+                                       dEdge('B', 'C'),
+                                       dEdge('C', 'A')
+                                   ],
+                                  [])
+                         ]))
